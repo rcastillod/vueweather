a52bd82ef3906116a52ab77c95f9099c6d3ebed5
rcastillod <rodrigo.castillo@global3.cl>
2022-08-31 21:04:12 -0400

    feat: Added skeleton loader to forecast cards

--- src/components/weather/WeatherForecast.vue
+++ src/components/weather/WeatherForecast.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="forecast-wrapper">
-        <v-progress-circular
-            v-if="loader"
-            indeterminate
-            color="rgba(255,255,255,.3)"
-            width="2"
-        ></v-progress-circular> 
-        <ul v-else class="forecast px-0 mt-12">
+        <ul class="forecast px-0 mt-12">
             <li 
                 v-for="(forecast, index) in weatherForecast"
                 :key="index"
                 class="forecast-item">
+                <v-skeleton-loader
+                v-if="loader"
+                    v-bind="attrs"
+                    class="mx-auto"
+                    min-height='147'
+                    type="text, avatar, text, text"
+                    elevation="2"
+                ></v-skeleton-loader>
                 <weather-day
+                v-else
                     :dt="weatherForecast[index].dt"
                     :temp="weatherForecast[index].temp"
                     :weather="weatherForecast[index].weather"
@@ -45,7 +48,7 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
     .forecast-wrapper {
         min-height: 9.375rem;
         position: relative;
@@ -55,6 +58,20 @@ export default {
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
+    }
+    .v-skeleton-loader {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        padding-inline: .625rem;
+        &__text {
+            &:nth-child(1) { grid-column: 2 / 3; }
+            &:nth-child(3) { grid-column: 1 / 2; }
+            &:nth-child(4) { grid-column: 3 / 4; }
+        }
+        &__avatar {
+            grid-column: 1 / -1;
+            justify-self: center;
+        }
     }
     .forecast {
         display: flex;
